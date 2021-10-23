92cbfd48e101f8a17030e48bf85e28aad300e758
Mila Siti Salamah <milasitisalamah22@gmail.com>
2021-10-23 17:03:43 +0800

    Tugas 1 Mobile

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        leading: Icon(Icons.menu),
+        title: Text(
+          'Hello Appbar - Mila Siti Salamah',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [Icon(Icons.search), Icon(Icons.more_vert)],
+      ),
+    );
+  }
+}
