44e74d8354c5bc1d0079a92acf8a4414c043f18f
Mila Siti Salamah <milasitisalamah22@gmail.com>
2021-11-06 19:19:29 +0800

    first commit: Tugas 2

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,17 @@ class Home extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         actions: [Icon(Icons.search), Icon(Icons.more_vert)],
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.grey,
+        margin: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+        padding: EdgeInsets.all(50),
+        child: Icon(
+          Icons.home,
+          color: Colors.blue.shade900,
+          size: 150,
+        ),
       ),
     );
   }
